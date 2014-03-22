3adba3fde23d75b3b31357acf202c949243f51da
Abraham Sangha <abraham.sangha@gmail.com>
2014-03-21 23:32:28 -0500

    Initial functionality

--- app/js/controllers/login_controller.js
+++ app/js/controllers/login_controller.js
@@ -1,11 +1,11 @@
-angular.module("app").controller('LoginController', function($scope, $location, AuthenticationService) {
-  $scope.credentials = { username: "", password: "" };
+angular.module("app").controller('LoginController', function($scope) {
+  $scope.calc = { timing: undefined, distance: undefined, pace: undefined };
 
-  var onLoginSuccess = function() {
-    $location.path('/home');
-  };
-
-  $scope.login = function() {
-    AuthenticationService.login($scope.credentials).success(onLoginSuccess);
+  $scope.calculate = function() {
+    var timing = $scope.calc.timing
+    var distance = $scope.calc.distance
+    var pace = $scope.calc.pace
+    $scope.calc.pace = timing / distance
+    $scope.calc.distance = timing / pace
   };
 });
